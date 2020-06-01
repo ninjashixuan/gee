06ee7f8898db554ba15a4a6ead12f7bfd872fa97
shixuan <zheshixuan@163.com>
2020-06-01 20:02:26 +0800

    test orm

--- main.go
+++ main.go
@@ -1,78 +1,38 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"gee/geecache"
+	"database/sql"
 	"log"
-	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var db = map[string]string{
-	"Tom":  "630",
-	"Jack": "589",
-	"Sam":  "567",
-}
-
-func createGroup() *geecache.Group {
-	return geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(
-		func(key string) ([]byte, error) {
-			log.Println("[SlowDB] search key", key)
-			if v, ok := db[key]; ok {
-				return []byte(v), nil
-			}
-			return nil, fmt.Errorf("%s not exist", key)
-		}))
-}
-
-func startCacheServer(addr string, addrs []string, gee *geecache.Group) {
-	peers := geecache.NewHTTPPool(addr)
-	peers.Set(addrs...)
-	gee.RegisterPeers(peers)
-	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr[7:], peers))
-}
-
-func startAPIServer(apiAddr string, gee *geecache.Group) {
-	http.Handle("/api", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			key := r.URL.Query().Get("key")
-			view, err := gee.Get(key)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Write(view.ByteSlice())
-
-		}))
-	log.Println("fontend server is running at", apiAddr)
-	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
-
-}
-
 func main() {
-	var port int
-	var api bool
-	flag.IntVar(&port, "port", 8001, "Geecache server port")
-	flag.BoolVar(&api, "api", false, "Start a api server?")
-	flag.Parse()
+	db, err := sql.Open("sqlite3", "gee.db")
+	if err != nil {
+		panic("sql Open failed error: " + err.Error())
+	}
+	defer func() {
+		_ = db.Close()
+	}()
 
-	apiAddr := "http://localhost:9999"
-	addrMap := map[int]string{
-		8001: "http://localhost:8001",
-		8002: "http://localhost:8002",
-		8003: "http://localhost:8003",
+	_, _ = db.Exec("DROP TABLE IF EXISTS User;")
+	_, _ = db.Exec("CREATE TABLE User(Name text, Age integer);")
+	result, err := db.Exec("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam")
+	if err == nil {
+		affected, _ := result.RowsAffected()
+		log.Println(affected)
 	}
 
-	var addrs []string
-	for _, v := range addrMap {
-		addrs = append(addrs, v)
+	result, err = db.Exec("INSERT INTO User(`Name`, `Age`) values (?, ?), (?, ?)", "ninja", 12, "hello",14)
+	if err == nil {
+		affected, _ := result.RowsAffected()
+		log.Println(affected)
 	}
 
-	gee := createGroup()
-	if api {
-		go startAPIServer(apiAddr, gee)
+	row := db.QueryRow("SELECT Name FROM User LIMIT 1")
+	var name string
+	if err := row.Scan(&name); err == nil {
+		log.Println(name)
 	}
-	startCacheServer(addrMap[port], addrs, gee)
 }
